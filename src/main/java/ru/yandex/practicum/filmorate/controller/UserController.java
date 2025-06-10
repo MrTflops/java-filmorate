c88598f3a2c48e42a5ce627625d57e3f02da6d9f
Сысоев Егор Александрович <SysoevEA@hq.corp.mos.ru>
2025-06-10 16:59:12 +0300

    UserController - динаковые пути и хедеры  вынеынесены повторяющиеся пути в константы:с в переменную

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -14,33 +14,45 @@ import java.util.Collection;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+    // Константы для путей
+    private static final String ID_PATH = "/{id}";
+    private static final String FRIENDS_PATH = ID_PATH + "/friends";
+    private static final String COMMON_FRIENDS_PATH = FRIENDS_PATH + "/common/{otherId}";
+    private static final String FRIEND_ID_PATH = FRIENDS_PATH + "/{friendId}";
+
+    // Константы для логов
+    private static final String GET_USER_LOG = "Получен GET-запрос на получение пользователя по id {}.";
+    private static final String GET_ALL_USERS_LOG = "Получен GET-запрос на получение всех пользователей.";
+    private static final String GET_FRIENDS_LOG = "Получен GET-запрос на получение всех друзей пользователя {}.";
+    private static final String GET_COMMON_FRIENDS_LOG =
+            "Получен GET-запрос на получение общих друзей пользователя {} с пользователем {}.";
 
     private final UserService userService;
 
-    @GetMapping("/{id}")
+    @GetMapping(ID_PATH)
     public User findUserById(@PathVariable Long id) {
-        log.info("Получен GET-запроса на получение пользователя по id {}.", id);
+        log.info(GET_USER_LOG, id);
         return userService.findUserById(id);
     }
 
     @GetMapping
     public Collection<User> findAll() {
-        log.info("Получен GET-запрос на получение всех пользователей.");
+        log.info(GET_ALL_USERS_LOG);
         return userService.findAll();
     }
 
-    @GetMapping("/{id}/friends")
-    Collection<User> getFriends(@PathVariable Long id) {
-        log.info("Получен GET-запрос на получение всех друзей пользователя {}.", id);
+    @GetMapping(FRIENDS_PATH)
+    public Collection<User> getFriends(@PathVariable Long id) {
+        log.info(GET_FRIENDS_LOG, id);
         return userService.getFriends(id);
     }
 
-    @GetMapping("/{id}/friends/common/{otherId}")
-    Collection<User> getCommonFriends(
+    @GetMapping(COMMON_FRIENDS_PATH)
+    public Collection<User> getCommonFriends(
             @PathVariable("id") Long userId,
             @PathVariable Long otherId
     ) {
-        log.info("Получен GET-запрос на получение общих друзей пользователя {} с пользователем {}.", userId, otherId);
+        log.info(GET_COMMON_FRIENDS_LOG, userId, otherId);
         return userService.getCommonFriends(userId, otherId);
     }
 
@@ -56,23 +68,23 @@ public class UserController {
         return userService.update(user);
     }
 
-    @PutMapping("/{id}/friends/{friendId}")
+    @PutMapping(FRIEND_ID_PATH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void putFriend(
             @PathVariable("id") Long userId,
             @PathVariable Long friendId
     ) {
-        log.info("Получен PUT-запрос на добавления в друзья");
+        log.info("Получен PUT-запрос на добавление в друзья пользователя {} пользователю {}", friendId, userId);
         userService.putFriend(userId, friendId);
     }
 
-    @DeleteMapping("/{id}/friends/{friendId}")
+    @DeleteMapping(FRIEND_ID_PATH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteFriend(
             @PathVariable("id") Long userId,
             @PathVariable Long friendId
     ) {
-        log.info("Получен Delete-запрос на удаление друга");
+        log.info("Получен DELETE-запрос на удаление друга {} у пользователя {}", friendId, userId);
         userService.deleteFriend(userId, friendId);
     }
 }
